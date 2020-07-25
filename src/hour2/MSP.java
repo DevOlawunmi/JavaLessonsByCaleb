ad31f7dfcce53619cf9847439d44b12a2087df67
Ola <olawunmi263@yahoo.com>
2020-07-25 03:38:34 +0100

    Creating Objects

--- src/hour2/MSP.java
+++ src/hour2/MSP.java
@@ -2,9 +2,13 @@ package hour2;
 
 public class MSP {
     public static void main(String[] args) {
-        User user = new User();
-        user.name = "Shandy";
+        User user = new User(); // Here I instantiated the User class onto onto the new 'user' object' ie creating an instance of it
+        //doing this gives the user object access to properties and methods in the User class
+        user.name = "Shandy";// Here, I set the name property to Shandy
         System.out.println(user.name);
         System.out.println(user.fullName());
+        String x = "hello";
+
     }
+
 }
--- src/hour2/User.java
+++ src/hour2/User.java
@@ -1,5 +1,13 @@
 package hour2;
-/*When we create a variable inside a class and make it publicly accessible, it is known as a property
+/*
+Classes: In OOP, classes are often used to set the structure of something you might want to represent
+Could be people, transactions, rectangles etc.
+Classes then have properties(nouns) which store something and methods(verbs) which do something
+access modifiers eg public determine who has access to the class
+Static means no instance of the class needs to be created before the it can be used
+Static can be applied to variables, methods, blocks and nested classes
+
+When we create a variable inside a class and make it publicly accessible, it is known as a property
 This class has property of 'name'
 Basically, we're saying that every user created will be able to get a name.
 How do we create users?
