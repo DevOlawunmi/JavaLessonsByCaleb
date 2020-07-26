3a66f0578de44745274cba48d03871cdc84f6056
Ola <olawunmi263@yahoo.com>
2020-07-26 04:14:23 +0100

    Creating Objects

---
+++ src/hour2/BeginnerNotesBBCBiteSize.java
@@ -0,0 +1,20 @@
+package hour2;
+
+/*
+Computer programs use data types to organise different types of data in a program.
+Data can also be constant or variable within programs and functions. The main purpose of computer programs is to collect and process data.
+A computer can change what it does depending on the data that it has to work with.
+The data that is used in a program is referred to as the data values.
+These influence the sequence of instructions and the program outputs.
+The key role of a computer program is to store and process data.
+Any computer software has a data model that defines what data will be collected and worked on.
+ The data structure defines how the flow of data is controlled in relation to inputs, processes and outputs.
+
+
+
+PLEASE NOTE : The algorithm sets up the possibilities - it provides the set of instructions that can be executed.
+ The data fed into a program controls the sequence of instructions and decides what gets executed.
+ */
+
+public class BeginnerNotesBBCBiteSize {
+}
