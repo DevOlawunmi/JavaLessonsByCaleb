1a6c552fa92f4d7c9b33946eaa0890db86aca3b9
Ola <olawunmi263@yahoo.com>
2020-08-02 06:37:37 +0100

    Do wile Loop 1

--- src/loops/RepetitionStructuresDoWhile.java
+++ src/loops/RepetitionStructuresDoWhile.java
@@ -1,4 +1,21 @@
 package loops;
 
+import java.util.Scanner;
+
 public class RepetitionStructuresDoWhile {
+    public static void main(String[] args) {
+        String password = "Greatness";
+        String input;
+        do {
+            System.out.println("Please enter your password");
+            Scanner scanner = new Scanner(System.in);
+            input = scanner.nextLine();
+
+        } while (!(password.equals(input)));
+
+
+        System.out.println("Password correct");
+
+
+    }
 }
