901d2f4268c1fefe544c0bdd487658fe7a5b97e6
Ola <olawunmi263@yahoo.com>
2020-08-02 06:47:33 +0100

    Do while Loop 1 update

--- src/loops/RepetitionStructuresDoWhile.java
+++ src/loops/RepetitionStructuresDoWhile.java
@@ -5,13 +5,15 @@ import java.util.Scanner;
 public class RepetitionStructuresDoWhile {
     public static void main(String[] args) {
         String password = "Greatness";
+        Scanner scanner = new Scanner(System.in);
         String input;
         do {
             System.out.println("Please enter your password");
-            Scanner scanner = new Scanner(System.in);
+
             input = scanner.nextLine();
 
         } while (!(password.equals(input)));
+        scanner.close();
 
 
         System.out.println("Password correct");
