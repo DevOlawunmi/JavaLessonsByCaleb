4ec0b490eb0ddafd97cbd403f1c492fa3ac33673
Ola <olawunmi263@yahoo.com>
2020-08-03 04:21:42 +0100

    Nested for loops 3

--- src/loops/NestedForLoops2.java
+++ src/loops/NestedForLoops2.java
@@ -3,12 +3,8 @@ package loops;
 public class NestedForLoops2 {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 5; j++)
-                if (i == 7) {
-                    System.out.println("We're in "+ i);
-                    System.out.println("The current value of i is " + i + " J is currently " + j);
-                }
-            else System.out.println("The current value of i is " + i + " J is currently " + j);
+            for (int j = i; j < 5; j++)
+                System.out.print(" " + j);
         }
     }
 }
---
+++ src/loops/NestedForLoops3.java
@@ -0,0 +1,7 @@
+package loops;
+
+public class NestedForLoops3 {
+    public static void main(String[] args) {
+
+    }
+}
