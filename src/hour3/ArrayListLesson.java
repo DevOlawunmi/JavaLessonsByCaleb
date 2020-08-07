95f3c911c9c551659dc854c2b558c9d2f1de4285
Ola <olawunmi263@yahoo.com>
2020-08-07 14:30:21 +0100

    ArrayList Example

---
+++ src/hour3/ArrayListLesson.java
@@ -0,0 +1,15 @@
+package hour3;
+
+import java.util.ArrayList;
+
+public class ArrayListLesson {
+    public static void main(String[] args) {
+        ArrayList<String> cars = new ArrayList<>();
+        cars.add("Mazda");
+        cars.add("Volvo");
+        cars.add("Audi");
+        cars.add("Jaguar");
+        System.out.println(cars);
+
+    }
+}
