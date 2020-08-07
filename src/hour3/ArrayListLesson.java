154592966e50370a3d141a3147934705dc7a7b75
Ola <olawunmi263@yahoo.com>
2020-08-07 14:51:41 +0100

    ArrayList Example update

--- src/hour3/ArrayListLesson.java
+++ src/hour3/ArrayListLesson.java
@@ -11,5 +11,22 @@ public class ArrayListLesson {
         cars.add("Jaguar");
         System.out.println(cars);
 
+        System.out.println(cars.get(1));//output is volvo
+        cars.set(3, "Bentley"); // this replaces Jaguar with Bentley
+        cars.add("Ford");
+        cars.add("Buick");
+        cars.add("Jaguar");
+        System.out.println(cars);
+        System.out.println(cars.size());
+        ;
+
+        //  cars.remove(1); removes the element in that index
+        //  cars.clear(); gets rid of all elements in the array
+
+        for (int i = 0; i < cars.size(); i++)
+        {
+            System.out.println("I have a " + cars.get(i)); //this loops through the list and displays current index
+
+        }
     }
 }
